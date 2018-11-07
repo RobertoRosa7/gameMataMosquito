1c7c2f28df24cffe3c405735c5e2d451dfc0b4bb
RobertoRosa7 <roberto.rosa7@gmail.com>
2018-11-07 14:13:57 -0200

    posição randômica finalizada

--- jogo.js
+++ jogo.js
@@ -15,5 +15,29 @@ function ajustaTamanhoJogo(){
 	largura = window.innerWidth;
 
 	//degug
-	console.log(altura, largura);
+	console.log(largura, altura);
+}
+ajustaTamanhoJogo();
+
+
+// Função para encapsular os elementos de criação da página de forma programática
+function posicaoRondomica(){
+
+	// Criando posições randômicas para a imagem mosquito
+	var posicaoX = Math.floor(Math.random() * largura);
+	var posicaoY = Math.floor(Math.random() * altura);
+	console.log(posicaoX, posicaoY);
+
+	// Criando o elemento HTML de imagem
+	var mosquito = document.createElement('img');
+
+	// Adicionando atribuo src da imagem
+	mosquito.src = 'imagens/mata-mosquito-mosca.png';
+
+	// Adicionando Classe ao elemento
+	mosquito.className = 'mosquito1';
+
+	// Criando elemento filho do body
+	document.body.appendChild(mosquito);
+
 }
